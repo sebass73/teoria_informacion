4b715bb1edd01a5e8211cf83eb193f7dfe02a44d
Sebastián Martínez <smartinez@blitzit.com.ar>
2021-06-03 19:52:57 -0300

    Ajuste de codigo

--- src/Moneda.java
+++ src/Moneda.java
@@ -1,13 +1,11 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
-import java.time.Clock;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
-public class Montecarlo {
+public class Moneda {
     private static final int DECRECE=0;
     private static final int MANTIENE=1;
     private static final int CRECE=2;
@@ -17,7 +15,18 @@ public class Montecarlo {
     private double [][] matrizPasajeBTC = new double[3][3];
     private double [][] matrizPasajeETH = new double[3][3];
 
-    public Montecarlo(){
+    public class Nodo {
+        int valor;
+        double acierto;
+
+        public Nodo(Integer valor, double acierto) {
+            this.valor=valor;
+            this.acierto=acierto;
+        }
+    }
+
+    public Moneda(Scanner moneda){
+        this.cotizaciones = calcularCotizaciones(moneda);
     }
 
     public void printMatrizdePasaje(String moneda) throws FileNotFoundException {
@@ -32,17 +41,33 @@ public class Montecarlo {
         }
     }
 
-    public void calcularMatrizdePasaje(String moneda) throws FileNotFoundException {
-        Scanner input;
-        input = new Scanner(new File("C:/Users/juani/IdeaProjects/teoria_informacion/"+moneda));
+    public void printAutocorrelacion() {
+        double[] ac = autocorrelacion();
+        System.out.println("Autocorrelacion BTC: ");
+        for (int i=0; i< ac.length; i++)
+            System.out.println(i+": "+ac[i]);
+        System.out.println();
+    }
+
+    public void printCorrelacionCruzada(Moneda moneda){
+        double[] cc = moneda.correlacionCruzada(moneda);
+        System.out.println("Correlacion Cruzada: ");
+        for (int i=0; i< cc.length; i++)
+            System.out.println((i*50)+": "+cc[i]);
+        System.out.println();
+    }
+
+    public void calcularMatrizdePasaje() {
         double [][] matriz = new double[3][3];
         double [] exitos = {0,0,0};
-        int valor_anterior = Integer.parseInt(input.nextLine());
+        int valor_anterior;
         int valor_actual;
         int estado = MANTIENE;
 
-        while (input.hasNextLine()){
-            valor_actual = Integer.parseInt(input.nextLine());
+        for (int i=1; i<cotizaciones.size()-1;i++){
+            valor_anterior = cotizaciones.get(i-1);
+            valor_actual = cotizaciones.get(i);
+
             if (valor_actual > valor_anterior) {
                 matriz[CRECE][estado]++;
                 estado=CRECE;
@@ -56,7 +81,6 @@ public class Montecarlo {
                 estado=DECRECE;
             }
             exitos[estado]++;
-            valor_anterior = valor_actual;
         }
 
         for (int i = 0; i < matriz.length; i++) {
@@ -68,17 +92,23 @@ public class Montecarlo {
                     matrizPasajeETH[i][j] = Double.parseDouble(df.format(matriz[i][j]/exitos[j]));
             }
         }
-        input.close();
     }
 
-    public double[] autocorrelacion(String moneda) throws FileNotFoundException {
+    private List<Integer> calcularCotizaciones(Scanner input) {
+        List<Integer> arrayValues = new ArrayList<>();
+
+        while (input.hasNext()){
+            int valorActual = Integer.parseInt(input.nextLine());
+            arrayValues.add(valorActual);
+        }
+        return arrayValues;
+    }
+
+    private double[] autocorrelacion() {
         double [] vector = new double[51];
         for(int i=0; i<vector.length;i++)
             vector[i] = 0;
         int emisiones = 0;
-        Scanner input;
-        input = new Scanner(new File("C:/Users/juani/IdeaProjects/teoria_informacion/"+moneda));
-        List<Integer> cotizaciones = getCotizaciones(input);
         for (int i=0; i<cotizaciones.size(); i++){
             int valor1 = cotizaciones.get(i);
             emisiones++;
@@ -100,14 +130,8 @@ public class Montecarlo {
         for(int i=0; i<vector.length;i++)
             vector[i] = 0;
         int emisiones = 0;
-        Scanner inputBTC;
-        Scanner inputETH;
-        inputBTC = new Scanner(new File("C:/Users/juani/IdeaProjects/teoria_informacion/BTC"));
-        inputETH = new Scanner(new File("C:/Users/juani/IdeaProjects/teoria_informacion/ETH"));
-        List<Integer> cotizacionesBTC = getCotizaciones(inputBTC);
-        List<Integer> cotizacionesETH = getCotizaciones(inputETH);
-        for (int i=0; i<cotizacionesBTC.size(); i++){
-            int valor1 = cotizacionesBTC.get(i);
+        for (int i=0; i<cotizaciones.size(); i++){
+            int valor1 = cotizaciones.get(i);
             emisiones++;
             for(int j=i; j<=i+200 && j<cotizacionesETH.size();j=j+50){
                 int valor2 = cotizacionesETH.get(j);
@@ -143,7 +167,7 @@ public class Montecarlo {
 
         //EJECUCION DEL ALGORITMO
         while (!converge(pAnt, pAct, arrayLength)){
-            arrayValues.stream().forEach(value -> {
+            cotizaciones.stream().forEach(value -> {
                 sumaExitos(exitos, value);
                 n.getAndIncrement();
                 for(int i=0; i<arrayLength; i++){
@@ -157,7 +181,6 @@ public class Montecarlo {
                 }
             });
         }
-        input.close();
         return pAct;
     }
 
@@ -203,60 +226,5 @@ public class Montecarlo {
         }
         return true;
     }
+
 }
-/*
-       public void printMatrizdePasaje() throws FileNotFoundException {
-            for (int i = 0; i < matrizPasaje.length; i++) {
-                for (int j = 0; j < matrizPasaje[i].length; j++) {
-                    System.out.print(matrizPasaje[i][j] + " ");
-                }
-                System.out.println();
-            }
-        }
-
-    private void leerArchivo() throws FileNotFoundException {
-        //Scanner input = new Scanner(new File("/home/sebastian/Documents/ing_en_Sistemas/teoria_informacion/teoria_informacion/BTC"));
-        //Scanner input = new Scanner(new File("/home/sebastian/Documents/ing en Sistemas/teoria_informacion/tp_especial/ETH"));
-
-        Scanner input;
-        for (int n=0;n<2;n++){
-            input = new Scanner(new File("C:/Users/juani/IdeaProjects/teoria_informacion/BTC"));
-            if(n==1){
-                input = new Scanner(new File("C:/Users/juani/IdeaProjects/teoria_informacion/ETH"));
-            }
-
-            int valor_anterior = Integer.parseInt(input.nextLine());
-            int total = 0;
-            //Estados: 0="Decrece", 1="Mantiene", 2="Crece". Por default será "Mantiene"
-            int estado=1;
-
-            while (input.hasNextLine()) {
-                int valor_actual = Integer.parseInt(input.nextLine());
-                if (valor_actual > valor_anterior) {
-                    matrizPasaje[CRECE][estado]++;
-                    estado=CRECE;
-                }
-                if (valor_actual == valor_anterior) {
-                    matrizPasaje[MANTIENE][estado]++;
-                    estado=MANTIENE;
-                }
-                if (valor_actual < valor_anterior) {
-                    matrizPasaje[DECRECE][estado]++;
-                    estado=DECRECE;
-                }
-                total++;
-                valor_anterior = valor_actual;
-            }
-
-            for (int i = 0; i < matrizPasaje.length; i++) {
-                for (int j = 0; j < matrizPasaje[i].length; j++) {
-                    DecimalFormat df = new DecimalFormat("#.000");
-                    matrizPasaje[i][j]= Double.parseDouble(df.format(matrizPasaje[i][j]/total));
-                }
-            }
-            input.close();
-        }
-
-    }
-*/
-
--- src/main.java
+++ src/main.java
@@ -3,48 +3,58 @@ import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;*/
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
 import java.io.*;
-import java.util.List;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        Montecarlo montecarlo = new Montecarlo();
 
-        //montecarlo.printMatrizdePasaje();
+        Scanner inputBTC;
+        inputBTC = new Scanner(new File("./BTC"));
 
-        //Scanner input = new Scanner(new File("C:/Users/juani/IdeaProjects/teoria_informacion/BTC"));
-        //Scanner input = new Scanner(new File("/home/sebastian/Documents/ing en Sistemas/teoria_informacion/tp_especial/ETH"));
+        Scanner inputETH;
+        inputETH = new Scanner(new File("./ETH"));
 
+        Moneda ETH = new Moneda(inputETH);
+        Moneda BTC = new Moneda(inputBTC);
+
+        //System.out.println("Matriz de pasaje BTC:");
+        //BTC.calcularMatrizdePasaje();
+
+        //System.out.println("Matriz de pasaje ETH:");
+        //ETH.calcularMatrizdePasaje();
+
+        //BTC.printAutocorrelacion();
+        //ETH.printAutocorrelacion();
+
+        //BTC.printCorrelacionCruzada(ETH);
+
+    }
+}
         //List<Nodo> distProbabilidad = montecarlo.calcularDistribucionDeProbabilidades(input);
-
-        montecarlo.calcularMatrizdePasaje("BTC");
-        montecarlo.calcularMatrizdePasaje("ETH");
-        System.out.println("Matriz de pasaje BTC:");
-        montecarlo.printMatrizdePasaje("BTC");
-        System.out.println();
-        System.out.println("Matriz de pasaje ETH:");
-        montecarlo.printMatrizdePasaje("ETH");
+        /*System.out.println();
+        moneda.printMatrizdePasaje();
         System.out.println();
 
-        double[] acBTC = montecarlo.autocorrelacion("BTC");
-        System.out.println("Autocorrelacion BTC: ");
-        for (int i=0; i< acBTC.length; i++)
-            System.out.println(i+": "+acBTC[i]);
-        System.out.println();
-        double[] acETH = montecarlo.autocorrelacion("ETH");
+
+        double[] acETH = moneda.autocorrelacion("ETH");
         System.out.println("Autocorrelacion ETH: ");
         for (int i=0; i< acETH.length; i++)
             System.out.println(i+": "+acETH[i]);
         System.out.println();
 
-        double[] cc = montecarlo.correlacionCruzada();
+        double[] cc = moneda.correlacionCruzada();
         System.out.println("Correlacion Cruzada: ");
         for (int i=0; i< cc.length; i++)
             System.out.println((i*50)+": "+cc[i]);
         System.out.println();
 
-        /*DefaultPieDataset data = new DefaultPieDataset();
+        DefaultPieDataset data = new DefaultPieDataset();
         data.setValue("C", 40);
         data.setValue("Java", 45);
         data.setValue("Python", 15);
@@ -59,6 +69,6 @@ public class main {
         // Mostrar Grafico
         ChartFrame frame = new ChartFrame("JFreeChart", chart);
         frame.pack();
-        frame.setVisible(true);*/
+        frame.setVisible(true);
     }
-}
+}*/
